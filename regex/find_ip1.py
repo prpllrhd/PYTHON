109e23dd513800c75b12bd0126e2b0d9264247dc
Sammer Gawande <prpllrhd@yahoo.com>
2016-08-15 10:42:31 -0700

    new stuff

--- regex/find_ip1.py
+++ regex/find_ip1.py
@@ -5,6 +5,7 @@ f = open("hadoop-resourcemanager-jetty.log.2016_04_09")
 findip=re.compile(r"\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
 for i in f.readlines():
   a=re.search(findip,i)
+  print type(a)
   if d.has_key(a.group()):
     d[a.group()]+=1
   else:
